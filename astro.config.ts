4858771c1cd499456cf4209815274120d610b187
Bocxx <hello@bocxx.io>
2024-06-12 23:30:53 +0200

    Update - Tailwind V4

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,8 +9,5 @@ export default defineConfig({
     : 'http://localhost:4321',
   integrations: [
     react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
   ],
 })
